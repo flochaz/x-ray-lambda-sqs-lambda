4c564769cb9e7508a9be7ae69775db3e30724289
Florian CHAZAL <chazalf@amazon.com>
2020-10-23 14:16:53 +0200

    Initial commit

---
+++ bin/sqs-x-ray.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { SqsXRayStack } from '../lib/sqs-x-ray-stack';
+
+const app = new cdk.App();
+new SqsXRayStack(app, 'SqsXRayStack');
