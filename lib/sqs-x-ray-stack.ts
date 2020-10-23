f949fc5b7d43782eec8206c8adaa55e6769bf224
Florian CHAZAL <chazalf@amazon.com>
2020-10-23 18:48:11 +0200

    init commit

--- lib/sqs-x-ray-stack.ts
+++ lib/sqs-x-ray-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { LambdaToSqsToLambda } from "@aws-solutions-constructs/aws-lambda-sqs-lambda";
 
 export class SqsXRayStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new LambdaToSqsToLambda(this, 'LambdaToSqsToLambdaPattern', {
+      producerLambdaFunctionProps: {
+          runtime: lambda.Runtime.NODEJS_12_X,
+          handler: 'index.handler',
+          code: lambda.Code.fromAsset(`${__dirname}/lambda/producer-function`)
+      },
+      consumerLambdaFunctionProps: {
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: 'index.handler',
+        code: lambda.Code.fromAsset(`${__dirname}/lambda/consumer-function`)
+      }
+  });
   }
 }
